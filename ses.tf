5c97d0f72e7e0a8b749e46d5a08e8bbe738bbc04
Ricardo Teixeira <ricardoteix@gmail.com>
2023-07-22 19:12:45 -0300

    ses_endpoint transferido para o vpc_endpoint.tf

--- ses.tf
+++ ses.tf
@@ -27,18 +27,3 @@ resource "aws_iam_user_policy_attachment" "ses-attach" {
   user       = aws_iam_user.smtp_user.name
   policy_arn = aws_iam_policy.ses_sender.arn
 }
-
-# Endpoint
-resource "aws_vpc_endpoint" "ses_endpoint" {
-  vpc_id              = aws_vpc.vpc-projeto.id
-  service_name        = "com.amazonaws.${var.regiao}.email-smtp"
-  vpc_endpoint_type   = "Interface"
-
-  private_dns_enabled = true
-  security_group_ids  = [aws_security_group.sg_projeto_ses.id]
-  subnet_ids          = [aws_subnet.sn-projeto-private-2.id]  # Select the appropriate private subnet
-
-  tags = {
-    Name = "ses-endpoint"
-  }
-}
